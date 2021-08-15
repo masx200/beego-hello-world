0d52e5671355924b27260d1d909aeadebbec15f2
masx200 <34191203+masx200@users.noreply.github.com>
2021-08-15 15:14:17 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,13 +3,25 @@ package main
 import (
 	beego "github.com/beego/beego/v2/server/web"
 
+	cors "github.com/beego/beego/v2/server/web/filter/cors"
 	helloworld "hello/helloworld"
 	index "hello/index"
 	list "hello/list"
 	routers "hello/routers"
 )
 
+func addcors() {
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins :true,
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+}
 func main() {
+	addcors()
 	list.List()
 	routers.Init()
 	helloworld.Helloworld()
